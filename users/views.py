8fe508d004f8d2f2ce715a8f3c223f3f043c2703
Mansur-09595 <mansur_musaev_1997@mail.ru>
2021-03-01 00:59:40 +0300

    first commit

---
+++ users/views.py
@@ -0,0 +1,16 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
+
+from .forms import CustomUserCreationForm
+
+
+# Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+# Create your views here.
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'

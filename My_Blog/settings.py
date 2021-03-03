8bf98a7377d8f549cb929053bc9ca88173ce5096
Mansur-09595 <mansur_musaev_1997@mail.ru>
2021-03-04 00:02:20 +0300

    Sett

--- My_Blog/settings.py
+++ My_Blog/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]

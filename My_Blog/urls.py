0ffc3903c4f4172bfce89cbeb3bacef80b30d0fa
Mansur-09595 <mansur_musaev_1997@mail.ru>
2021-03-04 02:26:32 +0300

    Done

--- My_Blog/urls.py
+++ My_Blog/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve 
 
 
 urlpatterns = [
@@ -26,6 +26,5 @@ urlpatterns = [
     path('articles/', include('articles.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('django.contrib.auth.urls')),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),   
 ]
-
-urlpatterns += staticfiles_urlpatterns

9a05ada8cc00d0d96dff19a89012378c31bfb201
Mansur-09595 <mansur_musaev_1997@mail.ru>
2021-03-04 00:07:41 +0300

    Sett

--- My_Blog/urls.py
+++ My_Blog/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -22,4 +24,4 @@ urlpatterns = [
     path('articles/', include('articles.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('django.contrib.auth.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

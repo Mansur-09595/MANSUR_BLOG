a064eba73d54fbd0aa687d600fbe7e0462f7a7f6
Mansur-09595 <mansur_musaev_1997@mail.ru>
2021-03-04 21:28:16 +0300

    Heroku

--- My_Blog/settings.py
+++ My_Blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = secret_key
 #SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['musaevblog.herokuapp.com']
 #ALLOWED_HOSTS = []
@@ -147,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = email_username
-EMAIL_HOST_PASSWORD = email_password
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER', email_username)
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS', email_password)

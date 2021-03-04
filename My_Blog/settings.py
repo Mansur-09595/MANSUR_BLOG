9c6b2d380df3c1beb2f4238b3b6c2dfab50d13a8
Mansur-09595 <mansur_musaev_1997@mail.ru>
2021-03-04 08:38:18 +0300

    Done

--- My_Blog/settings.py
+++ My_Blog/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/musaevblog.herokuapp.com/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 

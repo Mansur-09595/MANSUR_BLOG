ad1a0737e12f73251e1be65e4a90a5bad885b219
Mansur-09595 <mansur_musaev_1997@mail.ru>
2021-03-04 20:12:25 +0300

    Heroku

--- My_Blog/settings.py
+++ My_Blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['musaevbloging.herokuapp.com']
+ALLOWED_HOSTS = ['musaevblog.herokuapp.com']
 
 
 # Application definition

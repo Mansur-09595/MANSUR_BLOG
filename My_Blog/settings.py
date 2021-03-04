8689aa5bbf4c53e040c38abc02b8cd60ec712af9
Mansur-09595 <mansur_musaev_1997@mail.ru>
2021-03-04 21:21:34 +0300

    Heroku

--- My_Blog/settings.py
+++ My_Blog/settings.py
@@ -147,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER', email_username)
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS', email_password)
+EMAIL_HOST_USER = email_username
+EMAIL_HOST_PASSWORD = email_password

53b6a4d0d1ab05318931d7c7fa220de0ce3773c7
Mansur-09595 <mansur_musaev_1997@mail.ru>
2021-03-04 15:18:24 +0300

    Done

--- My_Blog/urls.py
+++ My_Blog/urls.py
@@ -29,6 +29,4 @@ urlpatterns = [
 ]
 
 if not settings.DEBUG:
-    urlpatterns += static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
-    urlpatterns += static(settings.STATICFILES_DIRS, document_root=settings.STATICFILES_DIRS)

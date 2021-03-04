7fcb96848a784d643675cf0d5127880d10eca7fd
Mansur-09595 <mansur_musaev_1997@mail.ru>
2021-03-04 08:43:04 +0300

    Done

--- My_Blog/urls.py
+++ My_Blog/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('articles/', include('articles.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('django.contrib.auth.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+]
+
+if.settings.DEBUG:
+    urlpatterns += + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
